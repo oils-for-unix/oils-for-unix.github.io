013c6cdc5725d05525f04924ddba33daaadabdf0
Andy C <andy@oilshell.org>
2025-06-12 13:34:28 -0400

    [oils-vim] Add side by side
    With ImageMagick

--- run.sh
+++ run.sh
@@ -14,4 +14,24 @@ write-index() {
     > $HTML_DIR/index.html
 }
 
+trim-each() {
+  mkdir -p _tmp
+  set -x
+  for stage in 1 2 3; do
+    # from Claude AI: -fuzz 10% -trim +repage works
+    # then use -crop to remove the right
+    convert oils-vim/screenshots/stage$stage-demo.png \
+      -fuzz 10% -trim +repage -crop '400x0+0+0' _tmp/$stage.png
+  done
+}
+
+side-by-side() {
+  # I tried -resize '80%' 
+  # But it makes the image BIGGER and fuzzier
+  # Do it in <img> tag instead
+  convert _tmp/{1,2,3}.png \
+    +append \
+    +repage oils-vim/screenshots/side-by-side.png
+}
+
 "$@"
